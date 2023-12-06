801ab0f08e8b1320d5600251476260f701dd6d9e
JoshT1984 <warlord84502@yahoo.com>
2023-12-06 11:56:03 -0600

    Code Along

---
+++ catServer.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var http = require('http');
+var port = process.env.PORT || 8000;
+
+var server = http.createServer(function(req, res) {
+  if (req.method === 'GET' && req.url === '/cats') {
+    let meow = 'meow';
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(meow);
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
